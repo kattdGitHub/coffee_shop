216412f62a4f401b9ac12097ca538043f94fcc91
Sukh <offcial@User.Com>
2024-02-28 23:18:33 +0530

    3days

--- lib/dashboard_screen/BeanDetailsScreen.dart
+++ lib/dashboard_screen/BeanDetailsScreen.dart
@@ -8,89 +8,92 @@ class BeanDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.sizeOf(context).height * 0.6,
-            child: Stack(
-              children: [
-                SizedBox(
-                  height: MediaQuery.sizeOf(context).height * 0.6,
-                  width: MediaQuery.sizeOf(context).width,
-                  child: const Image(
-                    fit: BoxFit.fill,
-                    image: AssetImage(
-                      'assets/bean.jpeg',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.6,
+              child: Stack(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.sizeOf(context).height * 0.6,
+                    width: MediaQuery.sizeOf(context).width,
+                    child: const Image(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        'assets/bean.jpeg',
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 0.w,
-                  child: Container(
-                    padding: EdgeInsets.all(15.w),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(12.r),
-                      ),
-                      color: Color(0xff000000).withOpacity(0.4),
-                    ),
-                    width: MediaQuery.sizeOf(context).width,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 3,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Robusta Beans',
-                                style: TextStyle(
-                                  fontSize: 24.sp,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Text(
-                                "Frome Africa",
-                                style: TextStyle(
-                                  fontSize: 12.sp,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
+                  Positioned(
+                    bottom: 0.w,
+                    child: Container(
+                      padding: EdgeInsets.all(15.w),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(12.r),
                         ),
-                        Expanded(
-                          flex: 2,
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(padding: EdgeInsets.all(7),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(6.r),
-                                      ),
-                                      color: Color(0xff141921),
-                                    ),
-                                    child: Column(
-                                      children: [
-                                        Icon(
-                                          Icons.sports_football,
-                                          color: Colors.orange,
-                                        ),
-                                        Text(
-                                          "Beans",
-                                          style: TextStyle(
-                                            color: Colors.grey,
-                                          ),
-                                        )
-                                      ],
-                                    ),
+                        color: Color(0xff000000).withOpacity(0.4),
+                      ),
+                      width: MediaQuery.sizeOf(context).width,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Robusta Beans',
+                                  style: TextStyle(
+                                    fontSize: 24.sp,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
                                   ),
-                                  Container(
-                                    padding: EdgeInsets.all(7),
+                                ),
+                                Text(
+                                  "Frome Africa",
+                                  style: TextStyle(
+                                    fontSize: 12.sp,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(7),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(6.r),
+                                        ),
+                                        color: Color(0xff141921),
+                                      ),
+                                      child: Column(
+                                        children: [
+                                          Icon(
+                                            Icons.sports_football,
+                                            color: Colors.orange,
+                                          ),
+                                          Text(
+                                            "Beans",
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(7),
                                       decoration: BoxDecoration(
                                         borderRadius: BorderRadius.all(
                                           Radius.circular(6.r),
@@ -105,53 +108,203 @@ class BeanDetailsScreen extends StatelessWidget {
                                           ),
                                           Text(
                                             "Africa",
-                                            style:
-                                                TextStyle(color: Colors.grey),
+                                            style: TextStyle(color: Colors.grey),
                                           )
                                         ],
-                                      ),)
-                                ],
-                              ),SizedBox(height: 12),
-                              Container(
-                                alignment: Alignment.center,
-                                height: 50.h,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10.r),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(height: 12),
+                                Container(
+                                  alignment: Alignment.center,
+                                  height: 50.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(10.r),
+                                    ),
+                                    color: Color(0xff141921),
                                   ),
-                                  color: Color(0xff141921),
+                                  child: Text(
+                                    "Medium Roasted",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Description",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "The word and its Germanic cognates have existed in common use in West Germanic languages since before the 12th century,referring to broad beans, chickpeas, and other pod-borne seeds. This was long before the New World genus Phaseolus was known in Europe",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10.sp,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Container(
+                    child: Text(
+                      "Size",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Container(
+                     padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.r),
+                        color: Colors.grey,
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            "250gm",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8.w),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.r),
+                            color: Colors.grey,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "500gm",
+                                style: TextStyle(
+                                  color: Colors.white,
                                 ),
-                                child: Text(
-                                  "Medium Roasted",
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              )
+                              ),
                             ],
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        Container(
+                        padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.r),
+                            color: Colors.grey,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "1000gm",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Text(
-            "Description",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 25.sp,
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Price",
+                    style: TextStyle(color: Colors.grey, fontSize: 20),
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "\$",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: Colors.deepOrangeAccent),
+                          ),
+                          Text(
+                            "10.50",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(padding: EdgeInsets.all(8),
+                        child: Center(
+                          child: Text(
+                            "Add To Cart",
+                            style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.r),
+                          color: Colors.deepOrangeAccent,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Text(
-            "The word and its Germanic cognates have existed in common use in West Germanic languages since before the 12th century,referring to broad beans, chickpeas, and other pod-borne seeds. This was long before the New World genus Phaseolus was known in Europe",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 14.sp,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
